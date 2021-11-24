299a393ef9b398c9898b4232ca477dc36cbf8f16
Nicolás Fernández Gil <nicofdezgil@outlook.com>
2021-11-24 13:32:11 +0000

    more code for player and card created

---
+++ src/Card.java
@@ -0,0 +1,10 @@
+public class Card {
+    private int number;
+    private String type;
+
+    //Create the object Card to be use in Table, Player and Dealer
+    public Card(int n, String t){
+        this.number = n;
+        this.type = t;
+    }
+}
--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
     private String name;
     private int score;
+    private List<Card> hand = new ArrayList<>(11);
 
     public Player(String name){
         this.name = name;
     }
-    public void winner(){
-        score++;
+
+    public void winner(){score++;}
+    public int getScore(){return score;}
+
+    public void askCard(Card e){
+        hand.add(e);
     }
-    public int getScore(){
-        return score;
-    }
+
 }
